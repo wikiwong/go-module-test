542ec3cc994f982212a413c7b504cb7b8860ee6c
Rob Wong <rwong@fanatics.com>
2018-09-14 14:34:15 -0600

    init commit

---
+++ printer/printer.go
@@ -0,0 +1,8 @@
+package printer
+
+import "fmt"
+
+// SayHi says hello with a hardcoded version that should be changed whenever the version is incremented
+func SayHi() {
+	fmt.Println("Hi from go-module-test! This is version 0")
+}
---
+++ printer_test.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/wikiwong/go-module-test/printer"
+)
+
+func TestPrinter(t *testing.T) {
+	printer.SayHi()
+}
